ce2b27273c6baf85572b8943f7daa89a4b8e3afb
shajwan2023 <shajwan.namiq2023@gmail.com>
2023-05-16 12:35:37 +0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import "../app.css";
+</script>
+
+
+
+navbar
+<slot />
+footer
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import TodoForm from '../components/TodoForm.svelte';
+	import { todos } from '../stores/todoStore.js';
+	import Todo from '../components/Todo.svelte';
+</script>
+
+<div class="text-slate-800 max-w-lg mx-auto placeholder-cyan-100">
+	<h1 class="text-center">My Todos</h1>
+
+	<TodoForm />
+
+	{#each $todos as todo}
+		<Todo {todo} index={todo.id} />
+	{/each}
+</div>
